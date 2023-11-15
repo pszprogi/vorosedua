5180285a1b24a688b69beb595445545ce3a4a01c
pszprogi <pszprogi@gmail.com>
2023-11-15 01:21:26 +0100

    Új képek feltöltve, illetve a gombok már teljes méretükben kattinthatóak.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="webpage/css/stiluslap_gomb.css" />
+<link rel="stylesheet" type="text/css" href="webpage/css/stylesheet_index.css" />
 </head>
 
 <body style="body">
@@ -10,16 +10,17 @@
 
 
  <div class="flex-elem row">
-    <div class="cell"><a href="webpage/grafikus.munkak.rajzok.html">Rólam</a></div>
+    <div><a href="webpage/grafikus.munkak.rajzok.html" class="cell">Rólam</a></div>
 
-    <div class="cell"><a href="presta/index.php">Webshop</a></div>
+    <div><a href="presta/index.php" class="cell">Webshop</a></div>
  </div>
 
 
  <div class="flex-elem row">
-    <div class="cell"><a href="webpage/gallery.php">Galéria</a></div>
+    <div ><a href="webpage/gallery.php" class="cell">Galéria</a></div>
 
-    <div class="cell"><a href="webpage/vendegkonyv.html">Kapcsolat</a></div>
+    <div ><a href="webpage/vendegkonyv.html" class="cell">Kapcsolat</a></div>
+
  </div>
 
  </div>
